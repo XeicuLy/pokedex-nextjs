cf8b5b0a94c6ba960d8a94dc894396f343d70edd
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-03-12 23:02:34 +0900

    :sparkles: feat: ポケモンのデータ取得機能追加
    axiosを使ってPokeAPIから必要な情報を取得し、変数に格納しました。

--- src/libs/pokemon.jsx
+++ src/libs/pokemon.jsx
@@ -1,1 +1,35 @@
-export const getAllPokemons = () => {};
+import axios from 'axios';
+import { BASE_URL } from '@/const/const';
+
+export const getAllPokemons = async () => {
+  // ポケモンの全データ取得
+  const res = await axios.get(BASE_URL).catch(() => {
+    return <h1>エラーが発生しました。</h1>;
+  });
+  const results = res.data.results;
+  const dataPromise = [];
+  for (let i = 0; i < results.length; i++) {
+    const result = results[i];
+    dataPromise.push(getPokemon(result.name));
+  }
+  return await Promise.all(dataPromise);
+};
+
+export const getPokemon = async id => {
+  // ポケモンの基本データ取得
+  const basicData = await axios.get(BASE_URL + id);
+  // ポケモンの詳細データ取得
+  const detailData = await axios.get(basicData.data.species.url);
+  // console.log(detailData);
+
+  await console.log(detailData);
+
+  const nameId = basicData.data.name;
+  const image = basicData.data.sprites.other['official-artwork']['front_default'];
+  const types = basicData.data.types;
+  const height = basicData.data.height;
+  const weight = basicData.data.weight;
+  const index = detailData.data.pokedex_numbers[0]['entry_number'];
+  const name = detailData.data.names[0].name;
+  const genus = detailData.data.genera[0].genus;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
+import { getAllPokemons } from '@/libs/pokemon';
 import type { NextPage } from 'next';
+import { useEffect, useRef } from 'react';
 
 const Home: NextPage = () => {
+  const didLogRef = useRef(false);
+  useEffect(() => {
+    if (didLogRef.current === false) {
+      didLogRef.current = true;
+      getAllPokemons();
+    }
+  }, []);
   return (
     <>
       <div className='text-center'>dictionary部分</div>
