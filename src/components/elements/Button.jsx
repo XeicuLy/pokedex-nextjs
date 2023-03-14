9da000cfe69b69f59af47233b3c17161552b18ba
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-03-14 14:37:13 +0900

    :recycle: refactor: ボタン改修

--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -1,12 +1,16 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export const Button = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.back();
+  };
   return (
     <>
       <div className='mx-auto my-2 mt-3 box-border w-1/4 rounded-md border-2 border-gray-800'>
-        <Link className='block h-full w-full' href={'/'}>
+        <button className='block h-full w-full' onClick={handleClick}>
           戻る
-        </Link>
+        </button>
       </div>
     </>
   );
