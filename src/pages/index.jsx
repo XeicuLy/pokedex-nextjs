ca6a6e064373758ae81fdf1d14cc9a1f5a61a0b9
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-03-19 21:46:39 +0900

    :art: style: スタイル適用

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,10 +16,11 @@ const Home = () => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
       const res = await getAllPokemon(BASE_URL);
-      console.log(res);
       // 各ポケモンの詳細データを取得
       loadPokemon(res.results);
+      // 次のページのURLをセット
       setNextURL(res.next);
+      // 前のページのURLをセット
       setPrevURL(res.previous);
       setLoading(false);
     };
@@ -35,7 +36,6 @@ const Home = () => {
     );
     setPokemonData(_pokemonData);
   };
-  console.log(pokemonData);
 
   const handlePrevPage = async () => {
     if (!prevURL) return;
@@ -48,6 +48,7 @@ const Home = () => {
   };
 
   const handleNextPage = async () => {
+    if (!nextURL) return;
     setLoading(true);
     const data = await getAllPokemon(nextURL);
     // console.log(data);
@@ -62,31 +63,39 @@ const Home = () => {
       <Head>
         <title>ポケモン図鑑</title>
       </Head>
-      <div>
+      <div className='mx-auto my-0 w-9/12 pt-6'>
         {loading ? (
           <h1 className='text-center'>ロード中...</h1>
         ) : (
           <>
-            <div className='grid grid-cols-4 grid-rows-5 gap-8 '>
+            <ul className='grid grid-cols-4 grid-rows-5 gap-8 '>
               {pokemonData.map((pokemon, i) => {
                 return <Card key={i} pokemon={pokemon} />;
               })}
-            </div>
+            </ul>
             <div className='flex content-center justify-center gap-8 py-6'>
-              <button
-                className='group relative h-12 w-48 overflow-hidden rounded-2xl bg-green-500 text-lg font-bold text-white'
-                onClick={handlePrevPage}
-              >
-                Prev
-                <div className='absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30'></div>
-              </button>
-              <button
-                className='group relative h-12 w-48 overflow-hidden rounded-2xl bg-green-500 text-lg font-bold text-white'
-                onClick={handleNextPage}
-              >
-                Next
-                <div className='absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30'></div>
-              </button>
+              {!prevURL ? (
+                <></>
+              ) : (
+                <button
+                  className='group relative h-12 w-48 overflow-hidden rounded-2xl bg-green-500 text-lg font-bold text-white'
+                  onClick={handlePrevPage}
+                >
+                  Prev
+                  <div className='absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30'></div>
+                </button>
+              )}
+              {!nextURL ? (
+                <></>
+              ) : (
+                <button
+                  className='group relative h-12 w-48 overflow-hidden rounded-2xl bg-green-500 text-lg font-bold text-white'
+                  onClick={handleNextPage}
+                >
+                  Next
+                  <div className='absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30'></div>
+                </button>
+              )}
             </div>
           </>
         )}
